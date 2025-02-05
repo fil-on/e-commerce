fa274f805ae452ff4f633ae522c0e07647809a2f
Fil <filongunio@gmail.com>
2025-02-05 16:26:37 +0800

    add edit page for updating product details

--- resources/views/livewire/product/index.blade.php
+++ resources/views/livewire/product/index.blade.php
@@ -53,7 +53,7 @@ class extends Component {
                                 class="font-bold">Price:</span> ${{
                             $product->price}}</p>
                         <div class="flex items-center justify-end w-full">
-                            <a href="#" wire:navigate
+                            <a href="{{ route('products.edit', $product->id) }}" wire:navigate
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                 Edit
                             </a>
