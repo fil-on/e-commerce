ba49593213b68193b900d889ce1579ca689dc3cf
Fil <filongunio@gmail.com>
2025-02-06 02:47:38 +0800

    add action message when updating cart

--- resources/views/livewire/product/list.blade.php
+++ resources/views/livewire/product/list.blade.php
@@ -46,7 +46,10 @@ class extends Component {
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                <x-action-message class="mb-5 me-3" on="cart-updated">
+                    {{ __('Cart Updated.') }}
+                </x-action-message>
+                <div class="grid gap-4 mb-10 md:grid-cols-2 lg:grid-cols-3">
                     @foreach ($products as $product)
                     <div wire:key="{{ $product->id }}"
                         class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
