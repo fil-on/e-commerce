79508a5c3ff4c0b843a53d341f1b4f8311e17ff4
Fil <filongunio@gmail.com>
2025-02-05 13:50:31 +0800

    add delete function to users page by admin

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -3,14 +3,19 @@
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 use App\Models\User;
+use Livewire\WithPagination;
 
 new #[Layout('layouts.app')] 
 class extends Component {
-
+    use WithPagination;
     public function with(): array {
         return [
-            'users' => User::all(),
+            'users' => User::paginate(9),
         ];
+    }
+
+    public function deleteUser($id) {
+        User::find($id)->delete();
     }
 }; ?>
 
@@ -20,13 +25,13 @@ class extends Component {
     </h2>
 </x-slot>
 
-<div class="py-12">
+<div class="py-12" x-data="{openModal: true}">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                     @foreach ($users as $user)
-                    <div
+                    <div wire:key="{{ $user->id }}"
                         class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{
@@ -41,18 +46,23 @@ class extends Component {
                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span
                                 class="font-bold">Role:</span> {{
                             $user->is_admin ? 'Admin' : 'User'}}</p>
-                        <a href="{{ route('users.edit', $user->id) }}"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Edit
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                        </a>
+                        <div class="flex items-center justify-end w-full">
+                            @if ($user->id !== auth()->user()->id)
+                            <a href="{{ route('users.edit', $user->id) }}" wire:navigate
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                Edit
+                            </a>
+                            <button type="button" wire:click="deleteUser({{ $user->id }})"
+                                wire:confirm="Are you sure you want to delete this user account?"
+                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                            @else
+                            <p class="text-sm italic text-gray-300">This is your account.</p>
+                            @endif
+                        </div>
                     </div>
                     @endforeach
                 </div>
+                {{ $users->links() }}
             </div>
         </div>
     </div>
