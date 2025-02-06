a0f8b0ba226e464928e7677144fa6c3b70aba0b3
Fil <filongunio@gmail.com>
2025-02-06 08:24:46 +0800

    add bottom margin

--- resources/views/livewire/product/index.blade.php
+++ resources/views/livewire/product/index.blade.php
@@ -35,7 +35,7 @@ class extends Component {
         </div>
         <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                <div class="grid gap-4 mb-10 md:grid-cols-2 lg:grid-cols-3">
                     @foreach ($products as $product)
                     <div wire:key="{{ $product->id }}"
                         class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
